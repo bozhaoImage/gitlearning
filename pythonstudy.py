adf169d7d372016b371d607505ab443b4ac3d469
赵博 <zhaobo@zhaobo.local>
2015-05-17 14:13:42 +0800

    study python

---
+++ pythonstudy.py
@@ -0,0 +1,20 @@
+from math import sqrt
+
+
+def functionRRR():
+    print 'Hello world'
+
+    if 1 == 0:
+        pass
+    else:
+        print 'I am right!'
+
+    print 'Linter'
+
+    if 1 == 0:
+        pass
+    else:
+        print sqrt(5.0)
+
+functionRRR()
+functionRRR()
